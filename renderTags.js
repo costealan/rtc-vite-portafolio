81fd96d27a081c39178915b19079058dc4ca05f1
cookingnoob <costealan@gmail.com>
2023-07-25 16:57:37 -0600

    agregue el cambio de estilo al contentContainer a las funciones para hacer el render del tag de la seccion

--- renderTags.js
+++ renderTags.js
@@ -3,10 +3,12 @@ import { contentContainer } from "./domElements";
 
 export const renderHomeTags = () => {
   const homeTags = homeTemplateTags();
+  contentContainer.classList.add('homePage');
   contentContainer.innerHTML += homeTags;
 }
 export const renderExperienceTags = () => {
   const experienceTags = experienceTemplateTags();
+  contentContainer.classList.add('experience')
   contentContainer.innerHTML += experienceTags;
 }
 
